5d078352c641310196642fffdecc9e608a8d5171
Victoria <victoria.rezbastos@gmail.com>
2021-09-03 20:37:22 -0300

    Inserindo novos módulos no projeto

--- app.js
+++ app.js
@@ -5,55 +5,20 @@
 // npm install -g nodemon
 // npm install ejs (renderização dos arquivos html, identifica arquivos js a serem executados pelo  servidor e arquivos html que retornam para o cliente) EJS cria arquivos do tipo html.
 // criar pasta app e dentro a pasta views()
+// criar pasta admin home e noticias dentro da views(para melhor organizar as pastas web)
+// Criar pasta public para assets, arquivos de montagem da página (CSS,JS(lado do cliente) e Imagens)
 
-const express = require('express');
-// App vai rodar o express, portanto não precisa criar server. Pula pro Listen.
-const app = express()
-
-// define o motor de views como sendo o EJS
-app.set('view engine', 'ejs')
-
-// configurar o caminho da pasta views, as vizualizações dos arquivos html
-app.set('views','./app/views')
-//ERRO Cannot get: nada está sendo retornado.
-
+const app = require('./config/server')
 // metodo get (endereço da rota, function req,res )
-// cria rota home
+// cria rota home, o '/' siginifica endereço puro, sempre leva pra home.
 app.get('/',(req,res) => {
-    res.send(// é como o res.end no node puro, mas no express é res.send
-        `<html>
-            <head>
-                <meta charset='UTF-8'>
-                <title>Portal de notícias</title>
-            </head>
-            <body>
-                <a href="/noticias">Notícias</a>
-                <h1>Portal de notícias</h1>
-            </body>
-        `
-) // o '/' siginifica endereço puro, sempre leva pra home.
+    // O EJS disponibiliza o metodo render para usar nas respostas das requisições.
+    // O método render localiza o arquivo index na pasta views e retorna seu conteúdo para o cliente. O restante do caminho já foi definido lá emcima em app.set('views'...)
+    res.render('home/index')
 })
 
 app.get('/noticias',(req,res)=>{
-    res.send(
-        `<html>
-            <head>
-                <meta charset='UTF-8'>
-                <title>Portal de notícias</title>
-            </head>
-            <body>
-                <a href="/">Home</a>
-                <h1>Todas as notícias</h1>
-                <ul>
-                    <li>Servidor Node.js com Express</li>
-                    <li>NODEMON melhora a performance do desenvolvedor NODE.JS</li>
-                    <li>Servidor Node.js com Express</li>
-                <ul>
-                
-            </body>
-        `
-    )
-
+    res.render('noticias/noticias')
 })
 
 app.listen(3000,()=>{ 
---
+++ config/server.js
@@ -0,0 +1,18 @@
+// Módulo (arquivo) de configuração do app
+
+const express = require('express');
+// App vai rodar o express, portanto não precisa criar server. Pula pro Listen.
+const app = express()
+
+// define o motor de views como sendo o EJS
+app.set('view engine', 'ejs')
+
+// configurar o caminho da pasta views, as vizualizações dos arquivos html
+app.set('views','./app/views')
+//ERRO Cannot get: nada está sendo retornado.
+
+// Configuração dos arquivos estáticos/assets --utilizando o metodo use da biblioteca express
+app.use(express.static('./app/public'))
+
+
+module.exports = app
